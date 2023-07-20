bf3beb6c212401a4b03cb4bfeb808bcf1ee61ec9
Waiyawat Mancharoen <waiyawatwork1@gmail.com>
2023-07-20 12:34:56 +0700

    index3 Responsive slide1

---
+++ script3.js
@@ -0,0 +1,19 @@
+function toggleham(x) {
+    x.classList.toggle("change");
+    
+    let mymenu = document.getElementById('mymenu')
+    if (mymenu.className === 'btn-menu') {
+        mymenu.className += 'menu-active'
+    }else {
+      mymenu.className = 'btn-menu';
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000
+});
+
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY >0);
+});
